dd591190bd25ea870979963189f7bc7e97df83f8
masoud zarepour <mdzarepour@gmail.com>
2025-01-27 23:35:23 +0330

    ""

--- lib/components/colors.dart
+++ lib/components/colors.dart
@@ -2,18 +2,16 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class SolidColors {
-<<<<<<< HEAD
   static Color backGroundColor = Color(0xff1C1F2E);
   static Color pinkButtonColor = Color(0xffF35383);
   static Color greyLinkColor = Color(0xffC5C5C5);
   static Color whiteColor = Color(0xffFFFFFF);
   static Color purpleColor = Color(0xffF98BFC);
   static Color blueColor = Color(0xff55B9F7);
-=======
+
   static Color backGroundPurple = Color(0xff1C1F2E);
   static Color pu8rpleButton = Color(0xffF35383);
   static Color grayLinkColor = Color(0xffC5C5C5);
->>>>>>> 1408d165d28e400de57e22bbeea2b4eb38231b23
 }
 
 class GradientColors {
@@ -21,13 +19,10 @@ class GradientColors {
     begin: Alignment.topLeft,
     end: Alignment.bottomRight,
     colors: [
-<<<<<<< HEAD
       Color.fromRGBO(255, 255, 255, 0.5),
       Color.fromRGBO(255, 255, 255, 0.2),
-=======
       Color.fromARGB(128, 255, 255, 255),
       Color.fromARGB(97, 255, 255, 255),
->>>>>>> 1408d165d28e400de57e22bbeea2b4eb38231b23
     ],
   );
 }
